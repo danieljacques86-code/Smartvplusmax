1d345396aeed541441c4faa7ce34e6a625119b1e
Plus-play <danieljacques86@gmail.com>
2026-04-24 08:14:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function assinar(){
+    window.open("https://testeagora.tv/logtvseries", "_blank");
+}
 
+function abrirTeste(){
+    document.getElementById("modal").style.display = "block";
+}
+
+function fechar(){
+    document.getElementById("modal").style.display = "none";
+}
